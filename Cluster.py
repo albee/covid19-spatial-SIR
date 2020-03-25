e02b97bcc3a3beee40dccea794442163cdf2eaf3
albee <albee@mit.edu>
2020-03-24 20:00:49 -0400

    dynamics working, got simple boston model

--- Cluster.py
+++ Cluster.py
@@ -1,15 +1,16 @@
 # Uses Kermack-McKendrick model, with spatial spread component between discrete steps
 import numpy as np
 from matplotlib import pyplot as plt 
+import time
 
 class Cluster(object):
 
 	def __init__(self, sim_iters=0, susceptible="0", infected="0", recovered="0", lat="45", lon="37", density="0"):
-		self.population = recovered + susceptible + infected
+		self.N = recovered + susceptible + infected
 
-		self.recovered = recovered
-		self.susceptible = susceptible
-		self.infected = infected
+		self.recovered = float(recovered)/self.N
+		self.susceptible = float(susceptible)/self.N
+		self.infected = float(infected)/self.N
 		self.time = 0
 
 		self.lat = lat
@@ -18,10 +19,11 @@ class Cluster(object):
 		self.density = density
 
 		self.history = np.zeros((sim_iters+1, 4))
-		self.history[0,:] = [self.time, susceptible, infected, recovered]  # [time, s, i, r]
+		self.history[0,:] = [self.time, self.susceptible, self.infected, self.recovered]  # [time, s, i, r]
 
-		self.beta = 2.5/14/1000  # infection rate (per day)
-		self.gamma = 1.0/14 # recovery rate (per day)
+		self.R0 = 1.7 #2.2  # R0 (approx)
+		self.gamma = 1.0/12.0 # recovery rate (per day): inverse of infectious duration
+		self.beta = self.R0*self.gamma # transmissibility x rate of contact (per day)
 
 		self.sim_iters = sim_iters
 		self.iter = 0
@@ -36,8 +38,8 @@ class Cluster(object):
 		s = self.susceptible
 		i = self.infected
 		r = self.recovered
-		beta = self.beta
 		gamma = self.gamma
+		beta = self.beta
 
 		# factors to add: density, "R0 goodness"
 
@@ -53,7 +55,8 @@ class Cluster(object):
 		self.susceptible = s
 		self.infected = i
 		self.recovered = r
-		print dr
+		print s, i, r
+		# time.sleep(0.5)
 
 		self.iter += 1
 		self.history[self.iter,:] = [self.time, s, i, r]
@@ -61,9 +64,9 @@ class Cluster(object):
 
 	def plot_cluster(self):
 		fig = plt.figure()
-		plt.plot(self.history[:,0], self.history[:,1])  # s
-		plt.plot(self.history[:,0], self.history[:,2])  # i
-		plt.plot(self.history[:,0], self.history[:,3])  # r
+		plt.plot(self.history[:,0], self.N*self.history[:,1])  # s
+		plt.plot(self.history[:,0], self.N*self.history[:,2])  # i
+		plt.plot(self.history[:,0], self.N*self.history[:,3])  # r
 		plt.legend(('susceptible', 'infected', 'recovered'))
 		plt.show()
 
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from Cluster import Cluster
 import math
 
-dt = 0.001 # days
-sim_iters = int(math.floor(30/dt))
+dt = 0.1 # days
+sim_iters = int(math.floor(180/dt))
 
-boston = Cluster(sim_iters, 10000, 1, 0)
+# boston = Cluster(sim_iters, 10000, 1, 0)
+boston = Cluster(sim_iters, 4500000, 10000, 0)
 
 for i in range(sim_iters):
 	boston.increment_model(dt)
-	print i
 
 boston.plot_cluster()
